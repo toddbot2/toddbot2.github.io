18bae1efee1a41cd1942d4917dfc2c40d1a77634
Todd Durboraw <toddurboraw@gmail.com>
2015-11-04 10:27:48 -0800

    more pics

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-	$('.click').click(function(){
+$(document).ready(function () {
+	$('a.gallery').colorbox({maxHeight:"80%", maxWidth: "90%", rel:"gallery", opacity: .7});
+  $('.click').click(function(){
 		console.log('hello friend');
 		var slide = $('.gallery').children('img', this);
+		console.log('thanks');
 		if (slide.hasClass('expand')){
 			slide.removeClass('expand')
 		}
 		else{
 			slide.addClass('expand');
 		}
-	});
-	$('.gallery').children('img').click(function(){
-		event.stopPropagation();
-	});
-});
+		console.log('all done');
 
+
+  	});  
+  
+  });
+
+  $('.click2').click(function(){
+  	console.log('hello')
+		var slide = $('.webstuff').children('a').children('img', this);
+		console.log('is this working?');
+		if (slide.hasClass('expand')){
+			slide.removeClass('expand');
+		}
+		else{
+			slide.addClass('expand');
+		}
+		$('img.webstuff').colorbox({height: 40});
+		console.log('all done');
+		});
+		
