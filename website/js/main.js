4500932c7f2a20f7f5b61ede45500c826041305c
Todd Durboraw <toddurboraw@gmail.com>
2017-03-08 20:59:55 -0800

    header

--- website/js/main.js
+++ website/js/main.js
@@ -1,5 +1,35 @@
-$(document).ready(function(){
-	$("#one, #two").stick_in_parent();
 
-	$(document.body).trigger("sticky_kit:recalc");
-});
+	// init controller
+	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+	// build scenes
+	new ScrollMagic.Scene({triggerElement: "#parallax1"})
+					.setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax2"})
+					.setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax3"})
+					.setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+					.addTo(controller);
+
+	
+	var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave', duration: "100%"
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("div.box");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				.addTo(controller);
+		};
---
+++ website/website_(current)/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$(".portrait").stick_in_parent();
+	$("#first").stick_in_parent();
+});
