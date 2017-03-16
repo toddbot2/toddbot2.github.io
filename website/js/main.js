38c7436b9a1ccc83e2d91c27715f1fda3839cc48
Todd Durboraw <toddurboraw@gmail.com>
2017-03-16 09:35:42 -0700

    header

--- website/js/main.js
+++ website/js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
 
 	$('.portrait')
 	.on('sticky_kit:bottom', function(e) {
-	    $(this).children("img").css('position', 'absolute');
+	    /*$(this).children("img").css('position', 'absolute');*/
 	    $(this).children("span").css('position', 'absolute');
 	    $(this).children("p").css('position', 'absolute');
-	    $(this).children("img").css('bottom', 0);
+	    /*$(this).children("img").css('bottom', 0);*/
 	    $(this).children("span").css('bottom', 0);
 	    $(this).children("p").css('bottom', 0);
 	})
 	.on('sticky_kit:unbottom', function(e) {
 	    $(this).children("span").css('position', 'static');
-	    $(this).children("img").css('position', 'static');
+	   /* $(this).children("img").css('position', 'static');*/
 	    $(this).children("p").css('position', 'static');
 	})
 	$(".portrait").stick_in_parent();
