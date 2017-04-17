6a45772d62a6810935d7b2a100975f0eb4db20b3
Todd Durboraw <toddurboraw@gmail.com>
2017-04-16 21:29:16 -0700

    header

---
+++ longform/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	$('.portrait')
+	.on('sticky_kit:bottom', function(e) {
+	    /*$(this).children("img").css('position', 'absolute');*/
+	    $(this).children("span").css('position', 'absolute');
+	    $(this).children("p").css('position', 'absolute');
+	    /*$(this).children("img").css('bottom', 0);*/
+	    $(this).children("span").css('bottom', 0);
+	    $(this).children("p").css('bottom', 0);
+	})
+	.on('sticky_kit:unbottom', function(e) {
+	    $(this).children("span").css('position', 'static');
+	   /* $(this).children("img").css('position', 'static');*/
+	    $(this).children("p").css('position', 'static');
+	})
+	$(".portrait").stick_in_parent();
+	$(".fullwidth").stick_in_parent();
+
+
+/*==============VIDEO STUFF====================*/
+// select video element
+    //var vid = document.getElementById('v0');
+    var vid = $('#v0')[0]; // jquery option
+
+    // pause video on load
+    vid.pause();
+
+    // pause video on document scroll (stops autoplay once scroll started)
+    window.onscroll = function(){
+        vid.pause();
+    };
+
+    // refresh video frames on interval for smoother playback
+    setInterval(function(){
+        vid.currentTime = window.pageYOffset/400;
+    }, 40);
+
+
+
+});
+
