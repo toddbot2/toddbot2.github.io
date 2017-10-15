45388f4b3097c60d3ac1efd13b424fa031783559
Todd Durboraw <toddurboraw@gmail.com>
2017-10-15 12:59:52 -0700

    header

--- chart/main.js
+++ chart/main.js
@@ -1,7 +1,7 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'horizontalBar',
 
     // The data for our dataset
     data: {
