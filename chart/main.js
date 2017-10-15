b85b5fac0c59cb7c1dd63564bb1d0146680117f7
Todd Durboraw <toddurboraw@gmail.com>
2017-10-14 23:43:22 -0700

    header

--- chart/main.js
+++ chart/main.js
@@ -1,21 +1,21 @@
-var ctx = document.getElementById('myChart1').getContext('2d');
+var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'horizontalBar',
+    type: 'line',
 
     // The data for our dataset
     data: {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
             label: "Is this thing working?",
-            backgroundColor: 'rgb(255, 99, 132,.4)',
+            backgroundColor: 'rgb(255, 99, 132,0.4)',
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45],
           }, {
           label: 'oranges',
       data: [2, 29, 5, 5, 2, 3, 10],
       backgroundColor: "rgba(255,153,0,0.4)"
-        },{
+    },{
       label: 'butt stuff',
       data: [10, 40, 0, 1, 17, 22, 50],
       backgroundColor: "rgba(255,30,110,0.4)"
