cad0791ca136226ba2840265057937c7c6b6d25d
Todd Durboraw <toddurboraw@gmail.com>
2017-10-30 22:48:56 -0700

    header

--- mega/js/main.js
+++ mega/js/main.js
@@ -54,7 +54,7 @@ var Navigation = $(".sec-nav");
     Header = $('#intro2').height();
 
 $(window).scroll(function() {
-  if( $(this).scrollTop() > Header +1090) {
+  if( $(this).scrollTop() > Header +1085) {
     Navigation.addClass(stickyDiv);
   } else {
     Navigation.removeClass(stickyDiv);
