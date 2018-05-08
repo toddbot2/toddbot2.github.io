d24ec7d9ad7f340a31cf6b7ae35c4450f788cba3
Todd Durboraw <toddurboraw@gmail.com>
2018-05-07 19:00:52 -0700

    header

--- elytra/js/main.js
+++ elytra/js/main.js
@@ -16,37 +16,6 @@ jQuery(document).ready(function($){
 		}
 	});
 
-$(document).scroll(function(e) {
-  var boxEnd = 4195;
-  
-  console.log(e.currentTarget.scrollingElement.scrollTop);
-  var currentScroll = e.currentTarget.scrollingElement.scrollTop;
-  //boxControl(boxId,heightShow,fixedHeight,heightLeave,currentHeight)
-  boxControl('#about-intro',835,'15%',boxEnd,currentScroll);
-  boxControl('#a',1500,'45vh',boxEnd,currentScroll);
-  boxControl('#b',2200,'45vh',boxEnd,currentScroll);
-  boxControl('#c',3800,'45vh',boxEnd,currentScroll);
-}) 
-
-function boxControl(boxId,heightShow,fixedHeight,heightLeave,currentHeight) {
-  $(boxId).css('top',fixedHeight);
-  var fixedHeightNumber = $(boxId).offset().top;
-  if(currentHeight > heightShow && currentHeight < heightLeave) {
-     $(boxId).css('position','fixed');
-     $(boxId).css('top',fixedHeight);
-     $(boxId).fadeIn();
-  } else if ( currentHeight >= heightLeave ) {
-     $(boxId).css('position','absolute');
-     $(boxId).css('top',heightLeave + fixedHeightNumber);
-     $(boxId).fadeIn();
-  } else {
-     $(boxId).fadeOut();
-  }
- 
-}
-
-
-new WOW().init();
 
 
 	$("body").prognroll({
