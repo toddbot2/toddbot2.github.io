997798663c36fbf53604936b1480402819f95d02
Todd Durboraw <toddurboraw@gmail.com>
2018-05-28 22:30:16 -0700

    header

--- mckenna/js/main.js
+++ mckenna/js/main.js
@@ -21,20 +21,20 @@ $(document).ready(function(){
 /*==============VIDEO STUFF====================*/
 // select video element
     //var vid = document.getElementById('v0');
-    var vid = $('#v0')[0]; // jquery option
+    /*var vid = $('#v0')[0];*/ // jquery option
 
     // pause video on load
-    vid.pause();
+    /*vid.pause();*/
 
     // pause video on document scroll (stops autoplay once scroll started)
-    window.onscroll = function(){
+    /*window.onscroll = function(){
         vid.pause();
-    };
+    };*/
 
     // refresh video frames on interval for smoother playback
-    setInterval(function(){
+    /*setInterval(function(){
         vid.currentTime = window.pageYOffset/400;
-    }, 40);
+    }, 40);*/
 
 
 
