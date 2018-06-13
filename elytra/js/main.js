6db559fd0cf97e409b6967c7e358ca1830be70b3
Todd Durboraw <toddurboraw@gmail.com>
2018-06-13 16:13:45 -0700

    header

--- elytra/js/main.js
+++ elytra/js/main.js
@@ -166,4 +166,50 @@ $('#main-post p, h2').each(function(){
     var string = $(this).html();
     string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
     $(this).html(string);
+
+
+
+/*====================BULLSHIT REACT SHIT====================*/
+
+var $cell = $('.card');
+
+//open and close card when clicked on card
+$cell.find('.js-expander').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  if ($thisCell.hasClass('is-collapsed')) {
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+    
+    if ($cell.not($thisCell).hasClass('is-inactive')) {
+      //do nothing
+    } else {
+      $cell.not($thisCell).addClass('is-inactive');
+    }
+
+  } else {
+    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+    $cell.not($thisCell).removeClass('is-inactive');
+  }
+});
+
+//close card when click on cross
+$cell.find('.js-collapser').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  $cell.not($thisCell).removeClass('is-inactive');
+
+});
+
+/*====================END BULLSHIT REACT SHIT====================*/
+
+
+
+
+
+
+
 });
